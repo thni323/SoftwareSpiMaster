c305f1f7c6bdbd774ddd21be3b1d42d25b68518f
thni323 <41083778+thni323@users.noreply.github.com>
2019-12-10 10:09:12 +0100

    Update spi.cpp

--- examples/spi.cpp
+++ examples/spi.cpp
@@ -2,23 +2,37 @@
 #include <SoftwareSpiMaster.h>
 
 // Slave select (chip select) pin
-const uint8_t SLAVE_SELECT = 5;
+const uint8_t SLAVE_SELECT = PIN_SS;
 
 SoftwareSpiMaster spi;
 
 void setup() {
-  spi.begin(12, 13, 4);
+  // Start spi
+  spi.begin(PIN_CLK, PIN_MOSI, PIN_MISO);
   
-  // Chip Select
-  pinMode(5, OUTPUT);
+  // Slave select acting as output
+  pinMode(PIN_SS, OUTPUT);
 }
 
 void loop() {
-  uint8_t readValue;
-  
-  // Read one byte from client by sending dummy data
+  // - Read one byte from slave by sending dummy data -
+  // Activate spi slave via chip select
   digitalWrite(SLAVE_SELECT, LOW);
+  // Transfer a single (dummy) byte
+  uint8_t readValue;
   readValue = spi.transfer(0xFF);
+  // Deactivate spi slave via chip select
+  digitalWrite(SLAVE_SELECT, HIGH);
+  
+  // - Transfer a buffer to the client - 
+  // Send and receive buffer initialization
+  uint8_t sendBuf[4] = { 0xFF, 0xFF, 0xFF, 0xFF };
+  uint8_t recBuf[4];
+  // Activate spi slave via chip select
+  digitalWrite(SLAVE_SELECT, LOW);
+  // Transfer sendBuf to client, received bytes get stored in recBuf
+  spi.transfer(sendBuf, recBuf, 4);
+  // Deactivate spi slave via chip select
   digitalWrite(SLAVE_SELECT, HIGH);
   
   delay(100);
